c8c23c25d33486ddd66e2ca735f0853c0e1c1fa8
mikierobertosito <tecnoworldschool@gmail.com>
2025-09-23 17:37:32 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,19 @@
+[
+    {
+        "titolo": "alessio e stato cacciato",
+        "descrizione": "bestemmiava e diceva parolacce",
+        "data": "oggi"
+    },
+    {
+        "titolo": "YOUTUBE",
+        "descrizione": "INIZIEREMO A FARE VIDEO SU YOUTUBE",
+        "data": ""
+    },
+    {
+        "titolo": "cacciati dal gruppo",
+        "descrizione": "si una nostra compagna di classe mi ha cacciato dal gruppo ma non è finita qui",
+        "data": "ieri"
+    }
+]
+
+
